7c4243da5aefc0e183cc6e34f22582c10d27acb1
planR <rplann24@gmail.com>
2026-02-18 13:49:44 +0000

    add a draw function and disable the mouseover

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const parentContainer = document.querySelector("#parent-container");
 const container = document.querySelector("#grid-container");
 const pixelBtn = document.querySelector("#pixel-btn");
 const colorPicker = document.querySelector("#color-picker");
+const eraser = document.querySelector("#eraser");
+
+
+createGrid(36);
+  draw();
 
 function createGrid(size) {
   if (size > 100 || size < 0 || !Number.isInteger(size)) {
@@ -11,24 +16,57 @@ function createGrid(size) {
      container.appendChild(para)
     return;
   }
+
   container.innerHTML = ' ';
   document.documentElement.style.setProperty("--p", size);
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement('div');
     square.classList.add('grid-square');
-    square.tabIndex = 0;
-    square.addEventListener('mouseleave', () => {
+
+   /* square.addEventListener('mouseover', (e) => {
         square.style.background = colorPicker.value;
-        console.log(square.key)
-    })
+    }); */
     container.appendChild(square);
   }
 };
-pixelBtn.addEventListener("input", () => {
-   const pixel = Number(pixelBtn.value);
-   createGrid(pixel);
-});
 
-document.addEventListener("keydown", (e) => {
-  console.log(e.key)
-})
+function draw() {
+  let squares = document.querySelectorAll(".grid-square");
+  let currentIndex = Math.floor(squares.length / 2);
+  squares[currentIndex].style.background = colorPicker.value;
+
+  container.tabIndex = 0;
+  container.focus();
+
+  container.addEventListener("keydown", (e) => {
+    if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.key)) {
+      e.preventDefault();
+    }
+
+    const squares = document.querySelectorAll('.grid-square');
+    let nextIndex = currentIndex;
+
+    switch (e.key) {
+      case 'ArrowUp': nextIndex = currentIndex - pixel; break;
+      case 'ArrowDown': nextIndex = currentIndex + pixel; break;
+      case 'ArrowLeft': nextIndex = currentIndex - 1; break;
+      case 'ArrowRight': nextIndex = currentIndex +1; break;
+    }
+
+    if (nextIndex >= 0 && nextIndex < squares.length) {
+      currentIndex = nextIndex;
+      squares[currentIndex].style.background = colorPicker.value;
+    }
+  });
+};
+let pixel = 36;
+//to get the pixel value/number/size
+pixelBtn.addEventListener("change", () => {
+   pixel = Number(pixelBtn.value);
+   createGrid(pixel);
+   draw();
+});
+eraser.addEventListener("click", () => {
+    createGrid(pixel || 36);
+    draw();
+});
